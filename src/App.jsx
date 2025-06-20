6a034d8aecf45b883511491b45337ad62b44c302
Ccasabianca <107222996+Ccasabianca@users.noreply.github.com>
2025-06-20 14:24:23 +0200

    formulaire axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = async () => {
+    const response = await axios.post("http://localhost:5173/tasks", {
+      name: task,
+    });
+    setTasks([...tasks, response.data]);
+    setTask("");
+  };
+
   return (
-    <>
-    </>
-  )
+    <div>
+      <h1>Task Manager</h1>
+      <input
+        type="text"
+        placeholder="Enter a task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={handleAddTask}>Add Task</button>
+      <ul>
+        {tasks.map((t) => (
+          <li key={t.id}>{t.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
